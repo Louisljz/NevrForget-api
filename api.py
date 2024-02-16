b5dcec6209bf6860eac027b173910841e65214dc
Louis JZ <louis.ljz08@gmail.com>
2024-02-17 00:40:44 +0700

    add convo history

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 
 from langchain.schema.output_parser import StrOutputParser
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.utils.function_calling import convert_to_openai_function
 
 import json
@@ -10,6 +11,7 @@ from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from datetime import datetime
 from pydantic import BaseModel, Field
+from typing import List
 from dotenv import load_dotenv
 
 
@@ -20,6 +22,9 @@ embeddings = OpenAIEmbeddings()
 output_parser = StrOutputParser()
 DB_PATH = "./chroma_db"
 
+class Message(BaseModel):
+    type: str
+    text: str
 
 class RetrieveMemory(BaseModel):
     """Call this to retrieve memories from the database"""
@@ -29,6 +34,30 @@ retrieve_func = convert_to_openai_function(RetrieveMemory)
 
 def retrieve_docs(query: str, retriever):
     return retriever.get_relevant_documents(query)
+
+def contextualize_query(chat_history, question):
+    messages = []
+    for message in chat_history:
+        if message.type == 'human':
+            messages.append(HumanMessage(message.text))
+        elif message.type == 'ai':
+            messages.append(AIMessage(message.text))
+
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}"),
+        ]
+    )
+    contextualize_q_chain = contextualize_q_prompt | model | output_parser
+    contextualize_query = contextualize_q_chain.invoke({"chat_history": messages, "question": question})
+    return contextualize_query
+
 
 async def stream_response(iterable):
     for item in iterable:
@@ -52,8 +81,11 @@ def summarize(chat: str):
 
 
 @app.post("/query")
-def query(question: str):
+def query(question: str, chat_history: List[Message]):
     retriever = Chroma(embedding_function=embeddings, persist_directory=DB_PATH, collection_name='memory').as_retriever()
+    if chat_history:
+        question = contextualize_query(chat_history, question)
+    
     prompt = ChatPromptTemplate.from_messages([
         ("system", "You are a dementia assistant, and your job is to refresh elderly memories by recalling events from the past and encourage them to storytell their day."),
         ("user", "{question}")
