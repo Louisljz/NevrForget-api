ce717d5fede7de40e69d464757dffde2774ab0be
Louis JZ <louis.ljz08@gmail.com>
2024-02-21 12:17:45 +0700

    metadata n semantic retrieval

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = FastAPI()
 tru = Tru()
-model = ChatOpenAI()
+model = ChatOpenAI(model='gpt-4-0125-preview')
 embeddings = OpenAIEmbeddings()
 output_parser = StrOutputParser()
 vector_store = Chroma(embedding_function=embeddings, persist_directory="./chroma_db", collection_name="memory")
@@ -32,11 +32,55 @@ class Message(BaseModel):
     type: str
     text: str
 
-class RetrieveMemory(BaseModel):
-    """Call this to retrieve memories from the database. For example, user activities, events, etc."""
-    query: str = Field(description="query for vector search")
+class SemanticRetrieval(BaseModel):
+    '''Call this to retrieve memories based on semantic search. For example, user activities, events, etc.'''
+    query: str
 
-retrieve_func = convert_to_openai_function(RetrieveMemory)
+def run_semantic_retrieval(query):
+    retriever = vector_store.as_retriever(search_kwargs={'k': 1})
+
+    template = """Answer the question based only on the following context: {context}
+    If there is a likely activity match, return the date as provided on the context.
+    Otherwise, respond with "I don't know".
+    {question}
+    """
+    rag_prompt = ChatPromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | rag_prompt
+        | model
+        | output_parser
+    )
+    rag_feedbacks = init_rag_feedbacks(rag_chain)
+    rag_recorder = TruChain(rag_chain,
+                            app_id='Conversational Retrieval Chain',
+                            feedbacks=rag_feedbacks)
+    
+    with rag_recorder:
+        answer = rag_chain.invoke(query)
+    return answer
+
+class MetadataRetrieval(BaseModel):
+    """Call this to retrieve memories based on metadata filtering by date."""
+    day: int
+    month: int
+    query: str = Field(description="question to ask about activities on the day")
+
+def run_metadata_retrieval(question, date_stamp):
+    res = vector_store._collection.get(where={'date_stamp': {'$eq': date_stamp}})
+    template = """Answer the question based only on the following context: {context}
+    If the context does not contain the answer, please respond with "I don't know".
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    chain = prompt | model | output_parser
+    answer = chain.invoke({"context": res['documents'][0], "question": question})
+    return answer
+
+semantic_retrieval = convert_to_openai_function(SemanticRetrieval)
+metadata_retrieval = convert_to_openai_function(MetadataRetrieval)
 
 def get_date_stamp(date):
     return int(datetime(date.year, date.month, date.day).timestamp())
@@ -59,7 +103,7 @@ def summarize(chat_history: List[Message]):
     messages = convert_chat(chat_history)
     summarize_prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", "You should summarize this conversation, by listing down memorable activities throughout the day. Write in bullet points."),
+            ("system", "You should summarize this conversation, by listing down memorable activities throughout the day. Write bullet points only."),
             MessagesPlaceholder(variable_name="chat_history"),
         ]
     )
@@ -81,39 +125,24 @@ def summarize(chat_history: List[Message]):
 def query(question: str, chat_history: List[Message]):
     messages = convert_chat(chat_history)
     general_prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are a dementia assistant, and your job is to refresh elderly memories by recalling events from the past and encourage them to storytell their day. Below is the chat history, use it as context."),
+        ("system", "You are a dementia assistant, and your job is to refresh elderly memories by recalling events from the past and encourage them to storytell their day."),
         MessagesPlaceholder(variable_name="chat_history"),
-        ("user", "Answer this question: {question}")
+        ("human", "{question}")
     ])
 
-    model_with_function = model.bind(functions=[retrieve_func])
+    model_with_function = model.bind(functions=[semantic_retrieval, metadata_retrieval])
     general_chain = general_prompt | model_with_function
-    response = general_chain.invoke({"question": question, "chat_history": messages})
+    response = general_chain.invoke({"question": question, 'chat_history': messages})
     
-    if response.additional_kwargs:
-        args = json.loads(response.additional_kwargs['function_call']['arguments'])
-        retriever = vector_store.as_retriever(search_kwargs={'k': 1})
-
-        template = """Answer the question based only on the following context: {context}
-        Question: {question}
-        If the context does not contain the answer, please respond with "I don't know".
-        """
-        rag_prompt = ChatPromptTemplate.from_template(template)
-
-        rag_chain = (
-            {"context": retriever | format_docs, "question": RunnablePassthrough()}
-            | rag_prompt
-            | model
-            | output_parser
-        )
-        rag_feedbacks = init_rag_feedbacks(rag_chain)
-        rag_recorder = TruChain(rag_chain,
-                                app_id='Conversational Retrieval Chain',
-                                feedbacks=rag_feedbacks)
-        
-        with rag_recorder:
-            answer = rag_chain.invoke(args['query'])
-        return answer
+    info = response.additional_kwargs
+    if info:
+        args = json.loads(info['function_call']['arguments'])
+        if info['function_call']['name'] == 'MetadataRetrieval':
+            date_stamp = int(datetime(2024, args['month'], args['day']).timestamp())
+            return run_metadata_retrieval(args['query'], date_stamp)
+        elif info['function_call']['name'] == 'SemanticRetrieval':
+            print('semantic retrieval')
+            return run_semantic_retrieval(question)
     else:
         return response.content
 
@@ -141,8 +170,8 @@ def base_query(question: str):
     retriever = vector_store.as_retriever(search_kwargs={'k': 1})
 
     template = """Answer the question based only on the following context: {context}
-    Question: {question}
     If the context does not contain the answer, please respond with "I don't know".
+    Question: {question}
     """
     rag_prompt = ChatPromptTemplate.from_template(template)
 
